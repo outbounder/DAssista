6a7725d6b2b0fad98fe791d0ede8526e38abe80c
outbounder <obiwonn@gmail.com>
2009-11-06 17:23:34 +0200

    fixes

--- tools/proxy/WGet.hx
+++ tools/proxy/WGet.hx
@@ -2,6 +2,7 @@ package haxe.org.dassista.tools.proxy;
 
 import haxe.org.dassista.IMultiModule;
 import haxe.org.dassista.IMultiModuleContext;
+import haxe.org.dassista.ModuleException;
 import haxe.rtti.Infos;
 
 import neko.io.Path;
@@ -24,19 +25,23 @@ class WGet implements IMultiModule, implements Infos
 		var cmdContext:IMultiModuleContext = context.clone();
 		cmdContext.set("root", context.get("root"));
 		cmdContext.set("cmd", "wget " + context.get("cmd"));
+		trace("wget " + context.get("cmd"));
 		return context.executeTargetModule("haxe.org.dassista.tools.proxy.Cmd", cmdContext) == 0;
 	}
 	
 	/**
 	 * @dest directory class path which will be used as destination of the download
 	 * @src any url (internet resource)
+	 * @name name of the outputfile
 	 * @return Bool
 	 */
 	public function download(context:IMultiModuleContext):Dynamic
 	{
+		if (!context.has("dest") || !context.has("src") || !context.has("name"))
+			throw "dest, src and name are needed";
 		var dest:String = context.getRealPath(context.get("dest"));
-		context.set("root", Path.directory(dest) );
-		context.set("cmd", "-O "+Path.withoutDirectory(dest)+" "+context.get("src"));
+		context.set("root", "" );
+		context.set("cmd", "-x -O "+dest+"\\"+context.get("name")+" "+context.get("src"));
 		return this.execute(context);
 	}
 }
