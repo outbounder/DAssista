e5319070fac047b2780dbc30262e047dfe15fd63
outbounder <obiwonn@gmail.com>
2009-11-19 17:19:21 +0200

    test prc upgrade to reflect changes

--- TestPrc.hx
+++ TestPrc.hx
@@ -1,24 +1,15 @@
 package haxe.org.dassista;
 
 import neko.Lib;
+import prc.Prc;
 
 class TestPrc
 {
 	public function new() { }
-	public static function main():Bool
+	public static function main():Void
 	{
-		var cmd:String = "haxe.exe --version";
-		var exec:Dynamic = Lib.load("Prc", "exec", 1);
-		try
-		{
-			var v = exec(untyped cmd.__s);
-			trace(v);
-		}
-		catch ( e : Dynamic )
-		{
-			throw "Process creation failure : " + e;
-		}
-		
-		return true;
+		var cmd:String = "haxe -lib prc -neko d:\\pd-repo\\haxe\\org\\dassista\\TestPrc.n -main haxe.org.dassista.TestPrc -D use_rtti_doc";
+		var prc:Prc = new Prc();
+		trace(prc.exec(cmd));
 	}
 }
